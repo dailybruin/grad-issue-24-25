7fc52b8e69c3facae408d399cb46fc8e6be2796a
Narek Germirlian <ngermirlian@gmail.com>
2025-05-29 18:21:57 -0700

    Initial landing commit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,24 +1,23 @@
 import React from "react";
 import styled from "styled-components";
-import GradIssuesLanding from "../images/bubbles.png"; // temporary landing
+import GradIssuesLanding from "../images/landing.gif"; // TODO: update landing
 
 const Container = styled.div`
   width: 100%;
-  height: 80vh;
   display: flex;
-  position: relative;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   background-color: black;
+  padding-top: 32px;
 `;
 
 const Background = styled.img`
   width: 100%;
   height: auto;
-  max-height: 100vh;
   object-fit: contain;
+  display: block; 
 `;
+
 
 const Landing = () => {
   return (
